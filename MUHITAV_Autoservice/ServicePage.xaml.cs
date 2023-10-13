6024b4b2750bb1efa4f446109ec681708e4a2a08
ShamidzeBar <rusichpanic@gmail.com>
2023-10-13 12:27:11 +0500

    5 лаба

--- MUHITAV_Autoservice/ServicePage.xaml.cs
+++ MUHITAV_Autoservice/ServicePage.xaml.cs
@@ -25,8 +25,28 @@ namespace MUHITAV_Autoservice
             InitializeComponent();
             var currentServices = МУХИТАОАОВ_автосервисEntities.GetContext().SERVESYS.ToList();
             ServiceListView.ItemsSource = currentServices;
+
+            ComboType.SelectedIndex = 0;
+
+            UpdateServices();
         }
 
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateServices();
+        }
+        private void ComboType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateServices();
+        }
+        private void RButtonDown_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateServices();
+        }
+        private void RButtonUp_Checked(object sender, RoutedEventArgs e)
+        {
+            UpdateServices();
+        }
         private void Button_c(object sender, RoutedEventArgs e)
         {
 
@@ -35,6 +55,48 @@ namespace MUHITAV_Autoservice
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Manager.MainFrame.Navigate(new AddEditPage());
+        }
+
+        private void UpdateServices()
+        {
+            var currentServices = МУХИТАОАОВ_автосервисEntities.GetContext().SERVESYS.ToList();
+
+            if(ComboType.SelectedIndex == 0)
+            {
+                currentServices = currentServices.Where(p => (p.Discount >= 0 && p.Discount <= 100)).ToList();
+            }
+            if (ComboType.SelectedIndex == 1)
+            {
+                currentServices = currentServices.Where(p => (p.Discount >= 0 && p.Discount <= 5)).ToList();
+            }
+            if (ComboType.SelectedIndex == 2)
+            {
+                currentServices = currentServices.Where(p => (p.Discount >= 5 && p.Discount <= 15)).ToList();
+            }
+            if (ComboType.SelectedIndex == 3)
+            {
+                currentServices = currentServices.Where(p => (p.Discount >= 15 && p.Discount <= 30)).ToList();
+            }
+            if (ComboType.SelectedIndex == 4)
+            {
+                currentServices = currentServices.Where(p => (p.Discount >= 30 && p.Discount <= 70)).ToList();
+            }
+            if (ComboType.SelectedIndex == 5)
+            {
+                currentServices = currentServices.Where(p => (p.Discount >= 70 && p.Discount <= 100)).ToList();
+            }
+
+            currentServices = currentServices.Where(p => p.Title.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+
+            if(RButtonDown.IsChecked.Value)
+            {
+                currentServices = currentServices.OrderByDescending(p => p.Cost).ToList();
+            }
+            if(RButtonUp.IsChecked.Value)
+            {
+                currentServices = currentServices.OrderBy(p => p.Cost).ToList();
+            }
+            ServiceListView.ItemsSource = currentServices;  
         }
     }
 }
